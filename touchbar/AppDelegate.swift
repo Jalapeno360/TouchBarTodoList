85b9d2d46f53e391e3eb1fbcdf931d06d8b5aa60
Alexis Hernandez <jalapeno9597@gmail.com>
2018-02-09 14:45:00 -0500

    Initial Commit

---
+++ touchbar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  touchbar
+//
+//  Created by Alexis Hernandez on 2/9/18.
+//  Copyright © 2018 Alexis Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ touchbar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  touchbar
+//
+//  Created by Alexis Hernandez on 2/9/18.
+//  Copyright © 2018 Alexis Hernandez. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
